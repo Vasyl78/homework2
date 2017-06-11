a6d7b8d8f3da4885288d31c031b1beca48806aff
admin <terekh.vasyl@gmail.com>
2017-06-11 21:00:06 +0300

    Created class 'Car'. Added methods 'initialize', 'speed_up', 'push_break' and class method 'default_car'. Created classes 'Desktop', 'Laptop' and module 'LinuxFriendly'. Prepended module in class 'Laptop'

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,26 @@
 
 # exercise object model
 module ObjectModel
+  # class desctop
+  class Desktop
+    def fork_process
+      'Parent: allocate memory'
+    end
+
+    def mine_bitcoins
+      inspect
+    end
+  end
+
+  # module linux friendly
+  module LinuxFriendly
+    def fork_process
+      super
+    end
+  end
+
+  # new class Leptop
+  class Laptop < Desktop
+    prepend LinuxFriendly
+  end
 end
--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -2,4 +2,31 @@
 
 # exercise state and behaviour
 module StateAndBehaviour
+  # class Car / Default car "Chargo"
+  class Car
+    attr_accessor :color, :year, :model, :current_speed
+
+    def initialize(params)
+      @color = params[:color] || 'red'
+      @model = params[:model] || 'Chargo'
+      @year = params[:year] || 1981
+      @current_speed = 0
+    end
+
+    def self.default_car
+      Car.new
+    end
+
+    def speed_up(speed)
+      @current_speed += speed
+    end
+
+    def push_break(speed)
+      if speed <= @current_speed
+        @current_speed -= speed
+      else
+        @current_speed
+      end
+    end
+  end
 end
